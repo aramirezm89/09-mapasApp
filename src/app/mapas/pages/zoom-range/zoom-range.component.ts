bf4e769fe6644ef1077d207dfa6ccdaa97edf2e9
Antonio Isaac Ramirez Monsalve <metal_invaders_21@hotmail.com>
2022-06-23 20:19:30 -0400

    ObtencionDecoordenadasDinamicaMente

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,21 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styleUrls: ['./zoom-range.component.css']
+  styleUrls: ['./zoom-range.component.css'],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 10;
+  center: [number, number] = [-70.703678, -33.52577];
+  constructor() {}
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
 
-  ngOnInit(): void {
-     var map = new mapboxgl.Map({
-       container: 'mapa',
-       style: 'mapbox://styles/mapbox/streets-v11',
-       center: [-70.703678, -33.52577],
-       zoom: 7.5,
-     });
+    //listener para el zoom del mapa
+    this.mapa.on('zoom', (event) => {
+      this.zoomLevel = this.mapa.getZoom();
+    });
+
+    this.mapa.on('zoomend', (event) => {
+      if (this.mapa.getZoom() > 18) {
+        this.mapa.zoomTo(18);
+      }
+    });
+
+    //listener para el movimiento del mapa
+    this.mapa.on('move', (evento) => {
+      const { lng, lat } = evento.target.getCenter();
+      this.center = [lng, lat];
+    });
   }
 
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () =>{});
+    this.mapa.off('zoomend', () =>{});
+    this.mapa.off('move', () =>{});
+  }
+
+  zoomOut() {
+    this.mapa.zoomOut();
+  }
+
+  zoomIn() {
+    this.mapa.zoomIn();
+  }
+
+  rangeZoom(valor: string) {
+    this.mapa.zoomTo(Number(valor));
+  }
 }
